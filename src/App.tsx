611062c4bf5512b4e1fc30f82c0ace4693d399e5
Devin <devinjc@udel.edu>
2023-02-14 11:53:43 -0500

    Fixed paragraph message

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,13 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">UD CISC275, Hello World</header>
+            <h1>This is header text</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Devin Cummings
             </p>
         </div>
     );
-}
-
-export function App(): JSX.Element {
-    return <h1>This is header text</h1>;
 }
 
 export default App;
