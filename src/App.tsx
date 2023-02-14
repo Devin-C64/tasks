aae3d3f3e16254b86624ad7d86dc18d1d2e77f75
Devin <devinjc@udel.edu>
2023-02-14 12:01:57 -0500

    Added unordered and ordered lists

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ function App(): JSX.Element {
             <header className="App-header">UD CISC275, Hello World</header>
             <h1>This is header text</h1>
             <p> Devin Cummings </p>
+            List of my classes:
+            <ul>
+                <li>CISC275</li>
+                <li>CISC304</li>
+                <li>CISC437</li>
+            </ul>
+            List of my favorite movies:
+            <ol>
+                <li>Star Wars</li>
+                <li>2001: A Space Odyssey</li>
+                <li>The Grand Budapest Hotel</li>
+            </ol>
         </div>
     );
 }
