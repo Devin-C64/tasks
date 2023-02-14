96f740323a2d18dd67904d813c70610ccc840745
Devin <devinjc@udel.edu>
2023-02-14 11:50:12 -0500

    Added paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">UD CISC275, Hello World</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Devin Cummings
             </p>
         </div>
     );
+}
+
+export function App(): JSX.Element {
+    return <h1>This is header text</h1>;
 }
 
 export default App;
