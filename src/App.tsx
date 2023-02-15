09b7aacfa55fd443f4595c6baf590ec343cefbff
Devin <devinjc@udel.edu>
2023-02-15 14:48:30 -0500

    Fixed headers so that they work with test cases. Final version

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">UD CISC275, Hello World</header>
-            <h1>This is header text</h1>
+            <header style={{ backgroundColor: "blue" }} className="App-header">
+                UD CISC275, Hello World
+            </header>
+            <h1>This is a header</h1>
             <p> Devin Cummings </p>
-            List of my classes:
-            <ul>
-                <li>CISC275</li>
-                <li>CISC304</li>
-                <li>CISC437</li>
-            </ul>
             List of my favorite movies:
             <ol>
                 <li>Star Wars</li>
                 <li>2001: A Space Odyssey</li>
                 <li>The Grand Budapest Hotel</li>
+                <li>Bullet Train</li>
             </ol>
             <img
                 src="https://preview.redd.it/j02itmog3t721.jpg?auto=webp&s=aae79e633e3d44b81580e1833ddce04a9ebc5bb3"
@@ -29,11 +26,23 @@ function App(): JSX.Element {
             </Button>
             <Container>
                 <Row>
-                    <Col>First column.</Col>
                     <Col>
-                        Second column. You can put whatever you want in here,
-                        and it will be on the right side. Maybe try adding an
-                        image?
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
                     </Col>
                 </Row>
             </Container>
