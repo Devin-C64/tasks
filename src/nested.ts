d2de349081e48dbd0b8b25cdeca51073d3fffa62
Devin <devinjc@udel.edu>
2023-02-28 18:02:28 -0500

    Added sumPublishedPoints function

--- src/nested.ts
+++ src/nested.ts
@@ -87,7 +87,10 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const array = [...questions];
+    const published = getPublishedQuestions(array);
+    const sum = sumPoints(published);
+    return sum;
 }
 
 /***
