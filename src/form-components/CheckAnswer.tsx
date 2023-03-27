897c5a9a5104477d793522abc6f9f395853c5cd0
Devin <devinjc@udel.edu>
2023-03-27 13:29:19 -0400

    Added CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control value={givenAnswer} onChange={updateAnswer} />
+            </Form.Group>
+            {givenAnswer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
